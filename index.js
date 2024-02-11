2e5a5abe81dc9020bb0465326b20bbd74840366c
Donaldicon <azihdonald@yahoo.com>
2024-02-11 23:09:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const slides = document.querySelector('.slides');
+
+let slideIndex = 0;
+
+prevBtn.addEventListener('click', () => {
+  slideIndex = (slideIndex - 1 + slides.children.length) % slides.children.length;
+  updateSlider();
+});
+
+nextBtn.addEventListener('click', () => {
+  slideIndex = (slideIndex + 1) % slides.children.length;
+  updateSlider();
+});
+
+function updateSlider() {
+  slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
